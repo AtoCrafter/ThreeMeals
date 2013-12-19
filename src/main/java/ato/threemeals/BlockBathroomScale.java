6c2d62033bf42105d58776c53a346c354535a97a
AtoCrafter <ato.crafter@gmail.com>
2013-12-19 23:15:00 +0900

    体重計の機能の実装

--- src/main/java/ato/threemeals/BlockBathroomScale.java
+++ src/main/java/ato/threemeals/BlockBathroomScale.java
@@ -2,25 +2,42 @@ package ato.threemeals;
 
 import net.minecraft.block.BlockBasePressurePlate;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class BlockBathroomScale extends BlockBasePressurePlate {
     protected BlockBathroomScale(int id) {
-        super(id, "planks_oak", Material.piston);
+        super(id, "emerald_block", Material.iron);
+        setHardness(0.5F);
+        setStepSound(soundMetalFootstep);
     }
 
     @Override
     protected int getPlateState(World world, int i, int j, int k) {
-        return 0;
+        float weight = 0;
+
+        List<Entity> list = world.getEntitiesWithinAABB(EntityPlayer.class, this.getSensitiveAABB(i, j, k));
+        if (list != null && !list.isEmpty()) {
+            for (Entity entity : list) {
+                if (entity instanceof EntityPlayer) {
+                    weight += ((HardcoreFoodStats)((EntityPlayer) entity).getFoodStats()).getFatness();
+                }
+            }
+        }
+
+        return (int) Math.min(weight / 10, 15);
     }
 
     @Override
     protected int getPowerSupply(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     protected int getMetaFromWeight(int i) {
-        return 0;
+        return i;
     }
 }
